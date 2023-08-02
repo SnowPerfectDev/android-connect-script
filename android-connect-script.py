9551972473117735772b749dae10d0c4d30826b9
Mercenario <83768808+SnowPerfectDev@users.noreply.github.com>
2023-08-02 17:04:01 -0300

    android-connect-script.py

--- android-connect-script.py
+++ android-connect-script.py
@@ -1,1 +1,18 @@
+import subprocess
+import time
+import sys
 
+while True:
+    try:
+        output = subprocess.check_output(['adb', 'connect', f'{sys.argv[1]}:5555'], stderr=subprocess.STDOUT, text=True)
+        if "connected" in output:
+            print("[!] SUCESSO!! Conectado a", f"{sys.argv[1]}:5555")
+            print("[+] pegando shell")
+            time.sleep(1)
+            subprocess.call(['adb', 'shell'], stderr=subprocess.DEVNULL)
+            sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        pass
+    
+    print("[*] Conectando...")
+    time.sleep(0.20)
